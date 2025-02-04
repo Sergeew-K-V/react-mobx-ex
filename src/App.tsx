53876d5d049e0907453da84ed2c12903995637c7
kirill.sergeev <kirill.sergeev@progforce.com>
2025-02-05 01:14:39 +0300

    root store example

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { observer } from "mobx-react-lite";
 import "./App.css";
 import postStore from "./store/post-store";
 import { useEffect } from "react";
+import { RootStoreContext } from "./root-store-context";
+import RootStore from "./store/root-store";
 
 const App = observer(() => {
   const { getPostsAction, posts } = postStore;
@@ -19,11 +21,13 @@ const App = observer(() => {
   }
 
   return (
-    <div className="app-class">
-      {posts?.value.map((el) => {
-        return <div>{JSON.stringify(el)}</div>;
-      })}
-    </div>
+    <RootStoreContext.Provider value={new RootStore()}>
+      <div className="app-class">
+        {posts?.value.map((el) => {
+          return <div>{JSON.stringify(el)}</div>;
+        })}
+      </div>
+    </RootStoreContext.Provider>
   );
 });
 
---
+++ src/store/root-store.ts
@@ -0,0 +1,7 @@
+import postStore from "./post-store";
+
+class RootStore {
+  post = postStore;
+}
+
+export default RootStore;
